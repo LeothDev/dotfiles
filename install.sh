b7f25137749ff01e5e836cec110686db5e3e0290
LeothDev <leonardonicolapalma@protonmail.com>
2025-03-21 22:34:40 +0100

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+DOTFILES_DIR=~/dotfiles
+
+# Create symlinks
+ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
+
+# Helix Config
+mkdir -p ~/.config
+ln -sf $DOTFILES_DIR/helix ~/.config/helix
+
+echo "Dotfiles installed!"
