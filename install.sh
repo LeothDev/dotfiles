6502352285bc8bdd22ee89e26b48f3e45975f805
LeothDev <leonardonicolapalma@protonmail.com>
2025-05-02 22:18:24 +0200

    [REFACTOR] Improved install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-DOTFILES_DIR=~/dotfiles
 
-# Create symlinks
-ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
-ln -sf $DOTFILES_DIR/.bash_aliases ~/.bash_aliases
-ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
+echo "Linking dotfiles..."
 
-ln -sf $DOTFILES_DIR/.config/alacritty ~/.config/alacritty
-ln -sf $DOTFILES_DIR/.config/fastfetch ~/.config/fastfetch
-ln -sf $DOTFILES_DIR/.config/helix ~/.config/helix
-ln -sf $DOTFILES_DIR/.config/hypr ~/.config/hypr
-ln -sf $DOTFILES_DIR/.config/ml4w ~/.config/ml4w
-ln -sf $DOTFILES_DIR/.config/ml4w-hyprland-settings ~/.config/ml4w-hyprland-settings
-ln -sf $DOTFILES_DIR/.config/rofi ~/.config/rofi
-ln -sf $DOTFILES_DIR/.config/waybar ~/.config/waybar
-ln -sf $DOTFILES_DIR/.config/starship.toml ~/.config/starship.toml
+DOTFILES_DIR="$HOME/dotfiles"
 
+# Symlink home dotfiles
+ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/.bash_aliases" "$HOME/.bash_aliases"
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 
+# Symlink config files
+mkdir -p "$HOME/.config"
+
+ln -sf "$DOTFILES_DIR/.config/alacritty" "$HOME/.config/alacritty"
+ln -sf "$DOTFILES_DIR/.config/fastfetch" "$HOME/.config/fastfetch"
+ln -sf "$DOTFILES_DIR/.config/helix" "$HOME/.config/helix"
+ln -sf "$DOTFILES_DIR/.config/hypr" "$HOME/.config/hypr"
+ln -sf "$DOTFILES_DIR/.config/ml4w" "$HOME/.config/ml4w"
+ln -sf "$DOTFILES_DIR/.config/ml4w-hyprland-settings" "$HOME/.config/ml4w-hyprland-settings"
+ln -sf "$DOTFILES_DIR/.config/rofi" "$HOME/.config/rofi"
+ln -sf "$DOTFILES_DIR/.config/waybar" "$HOME/.config/waybar"
+ln -sf "$DOTFILES_DIR/.config/starship.toml" "$HOME/.config/starship.toml"
+
+echo "Done!"
